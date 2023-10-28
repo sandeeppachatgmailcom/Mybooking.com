bbd3b89255536f13d7569842fcf118b11a125b5e
sandeep Pachat <sandeeppachat@gmail.com>
2023-10-28 11:47:21 +0530

    281020231147AM

--- asset/Script/customerSearch.js
+++ asset/Script/customerSearch.js
@@ -186,12 +186,17 @@ function readmore() {
 
   document.getElementById("idTariffDetails").innerHTML = `
   <div class="container-fluid" >
-    <div class="container-fluid d-flex  border btn" style="height :400px ;background-size: cover;background-position: center;">
-    <img src="${result.image1}" class="card-img-top col-4" alt="...">
-    <img src="${result.image2}" class="card-img-top col-4" alt="...">
-    <img src="${result.image3}" class="card-img-top col-4" alt="...">
-    
-    </div>
+    <div class="container-fluid d-flex flex-wrap border btn "  >
+      <div class="col-12 col-sm-4 col-md-4" style="height:400px;">
+          <img src="${result.image1}" class="card-img-top w-100 p-1 h-100" alt="...">
+      </div>
+      <div class="col-12 col-sm-4 col-md-4"style="height:400px;">
+          <img src="${result.image2}" class="card-img-top w-100 p-1 h-100" alt="...">
+      </div>
+      <div class="col-12 col-sm-4 col-md-4" style="height:400px;">
+          <img src="${result.image3}" class="card-img-top w-100 p-1 h-100" alt="...">
+      </div>
+  </div>
     <div class="container-fluid d-flex justify-content-evenly border btn" >
          <h6 class="card-title" style="text-transform: uppercase;">${result.firstName}</h6> 
     </div>     
